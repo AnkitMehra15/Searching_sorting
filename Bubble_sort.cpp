5552553e6d3b42cab0994a6fd1b9b8886a061a7a
AnkitMehra15 <143807999+AnkitMehra15@users.noreply.github.com>
2023-09-17 22:03:21 +0530

    Bubble_sort Source code

---
+++ Bubble_sort.cpp
@@ -0,0 +1,15 @@
+void Bubble_Sort(vector<int> &arr, int n){
+    bool swapped=false;
+    for(int i=1;i<n;i++)
+    {
+        for(int j=0;j<n-i;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+                swapped=true;
+            }
+        }
+        if(swapped==false){
+            break; //already sorted
+        }
+    }
+}
